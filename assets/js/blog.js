004e953532bed3ee29006b9346c81c8b217edf83
kylezap <kzapcic+github@gmail.com>
2024-03-27 16:00:39 -0400

    fixed logic in form so that all objects are stored locally

---
+++ assets/js/blog.js
@@ -0,0 +1,10 @@
+
+
+function createBlogCard(blogpost) {
+
+}
+
+
+for (blogpost of blogposts) {
+    let blogposts = localStorage.getItem(JSON.parse(blogposts))
+}
--- assets/js/form.js
+++ assets/js/form.js
@@ -5,9 +5,21 @@ const submitBlog = $('#submit-blog')
 
 let blogposts = [];
 
+//create read action to pull in local storage that already exists.
+function readProjectsFromStorage() {
+  
+    // TODO: Retrieve projects from localStorage and parse the JSON to an array. If there are no projects in localStorage, initialize an empty array and return it.
+    blogposts = JSON.parse(localStorage.getItem('blogposts'));
+  
+    if (blogposts == null) {
+      blogposts = [];
+    } return blogposts;
+  }
+
 function handleFormSubmit(event) {
     event.preventDefault();
     
+    readProjectsFromStorage();
 
     const blogpost = { 
         userName: usernameInput.val(),
@@ -17,7 +29,7 @@ function handleFormSubmit(event) {
    
     blogposts.push(blogpost);
 
-    localStorage.setItem('blogpostData', JSON.stringify(blogposts));
+    localStorage.setItem('blogposts', JSON.stringify(blogposts));
     
     window.location.href="blog.html";
     // blogpost.append(`<li>${shoppingItem}</li>`);
