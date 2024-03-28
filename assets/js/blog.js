8af3c579e822d21dac1161282ccbca2628b0fe6d
kylezap <kzapcic+github@gmail.com>
2024-03-27 22:25:17 -0400

    built blog posts page out, added dark-light button

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,10 +1,16 @@
+const rootEl = $('#root');
+let blogposts = [];
 
+blogposts = JSON.parse(localStorage.getItem('blogposts'));
 
-function createBlogCard(blogpost) {
-
-}
-
-
-for (blogpost of blogposts) {
-    let blogposts = localStorage.getItem(JSON.parse(blogposts))
+for (let i = 0; i < blogposts.length; i++) {
+    const cardEl = $('<div>').addClass('card d-grid gap-2 m-2');
+    const cardBodyEl = $('<div>').addClass('card-body');
+    const cardTitleEl = $('<h5>').text(blogposts[i].title).addClass('card-title');
+    const cardUserEl = $('<h6>').text(blogposts[i].userName).addClass('card-subtitle mb-2 text-muted');
+    const cardContentEl = $('<p>').text(blogposts[i].content).addClass('card-text');
+    
+    rootEl.append(cardEl);
+    cardEl.append(cardBodyEl);
+    cardBodyEl.append(cardTitleEl, cardUserEl, cardContentEl);
 }
